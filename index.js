8beaafd0832394f51ec5b7e712c7b2b9a0d7299e
NipunaDassanayake <nipuna920@gmail.com>
2025-02-04 17:58:20 +0530

    implements gitignore

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
 // Middleware to parse incoming JSON requests
@@ -26,8 +29,7 @@ app.use((req, res, next) => {
   next(); // pass the execution to the next middleware function
 });
 
-const mongoUrl =
-  "mongodb+srv://admin:123@cluster0.f3w4k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+let mongoUrl = process.env.MONGO_URL;
 
 // Connect to MongoDB
 mongoose.connect(mongoUrl);
