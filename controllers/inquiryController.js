9087bdb3d248abff05aa11e240705c5ffe6c98f6
NipunaDassanayake <nipuna920@gmail.com>
2025-03-17 12:09:39 +0530

    add new

--- controllers/inquiryController.js
+++ controllers/inquiryController.js
@@ -52,19 +52,73 @@ export async function getAllInquiries(req, res) {
 
 export async function deleteInquiry(req, res) {
   try {
+    const id = req.params.id;
+    let deletedInquiry;
+
     if (isItAdmin(req)) {
-      const id = req.params.id;
-      await Inquiry.deleteOne({ id: id });
-      res.json({ message: "Inquiry deleted successfully" });
+      deletedInquiry = await Inquiry.findOneAndDelete({ id: id });
     } else if (isItCustomer(req)) {
-      const id = req.params.id;
-      await Inquiry.deleteOne({ id: id, email: req.user.email });
-      res.json({ message: "Inquiry deleted successfully" });
+      deletedInquiry = await Inquiry.findOneAndDelete({
+        id: id,
+        email: req.user.email,
+      });
     } else {
-      res.status(403).json({ message: "Unauthorized to delete inquiries" });
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to delete inquiries" });
     }
+
+    if (!deletedInquiry) {
+      return res
+        .status(404)
+        .json({ message: "Inquiry not found or unauthorized" });
+    }
+
+    res.json({ message: "Inquiry deleted successfully" });
   } catch (error) {
     console.error("Error deleting inquiry:", error);
-    res.status(500).json({ message: "Failed to delete inquiry", error });
+    res.status(500).json({ message: "Failed to delete inquiry" });
+  }
+}
+
+export async function updateInquiry(req, res) {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+
+    if (isItAdmin(req)) {
+      const inquiry = await Inquiry.findByIdAndUpdate(id, data, { new: true });
+
+      if (!inquiry) {
+        return res.status(404).json({ message: "Inquiry not found" });
+      }
+      return res
+        .status(200)
+        .json({ message: "Inquiry updated successfully", inquiry });
+    }
+
+    if (isItCustomer(req)) {
+      const inquiry = await Inquiry.findOneAndUpdate(
+        { _id: id, email: req.user.email },
+        { message: data.message },
+        { new: true }
+      );
+
+      if (!inquiry) {
+        return res
+          .status(404)
+          .json({ message: "Inquiry not found or unauthorized" });
+      }
+      return res
+        .status(200)
+        .json({ message: "Inquiry updated successfully", inquiry });
+    }
+
+    return res
+      .status(403)
+      .json({ message: "Unauthorized to update inquiries" });
+  } catch (error) {
+    console.error("Error updating inquiry:", error);
+    res.status(500).json({ message: "Failed to update inquiry" });
   }
 }
--- routes/inquiryRouter.js
+++ routes/inquiryRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { addInquiry , getAllInquiries , deleteInquiry} from "../controllers/inquiryController.js";
+import { addInquiry , getAllInquiries , deleteInquiry, updateInquiry} from "../controllers/inquiryController.js";
 
 const inquiryRouter = express.Router();
 inquiryRouter.post("/", addInquiry);
 inquiryRouter.get("/", getAllInquiries);
 inquiryRouter.delete("/:id", deleteInquiry);
-
+inquiryRouter.put("/:id", updateInquiry);
 export default inquiryRouter;
